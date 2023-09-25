970962a68b34ee8c498147830ee83f62b1a7d49a
AliAksoyy <alifeyza4017@gmail.com>
2023-09-25 11:27:15 +0300

    #1 contuine and conditional events

--- jsCarshCourseBeginner/main.js
+++ jsCarshCourseBeginner/main.js
@@ -33,3 +33,72 @@ const person = {
   age: 30,
   isMarried: true,
 };
+
+// Operators
+// Assignment
+// Arithmetic
+// Comparison
+// Logical
+// String
+
+let x = 10;
+let y = 5;
+
+let xy = x > y;
+
+const isEven = 10 % 2 === 0 ? true : false;
+
+// Type Conversions
+// implicit conversion
+// explicit conversion
+
+let a = 2 + "3";
+console.log(true + "3");
+
+console.log("5" - true); // 4 true === 1
+console.log("a", 5 - undefined); // undefined olunca değer atanmadığı için olmuyor
+console.log(Number(""));
+console.log(typeof parseInt("ali") === "number");
+console.log(parseFloat("3.14"));
+console.log(String(null) == "null");
+console.log((20).toString(20));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN)); // null "" NaN 0
+
+// Equality
+// == (Allow coercion) (zorlama) değer kontrolü
+// === (Does not allow coercison) tip ve değer kontrolu yapıyor
+
+const var1 = 10;
+const var2 = "10";
+
+const var3 = "";
+const var4 = false;
+
+console.log(var1 == var2);
+console.log(var1 === var2);
+
+console.log(var3 == var4);
+console.log(var3 === var4);
+
+// Conditional Statament
+
+// if else else if swtich
+
+const num = -5;
+
+if (num > 0) {
+  console.log("number is positive");
+} else {
+  console.log("number is negative");
+}
+
+const number = 0;
+
+if (number > 0) {
+  console.log(number + " " + "is positive");
+} else if (number === 0) {
+  console.log(number + " " + "is 0");
+} else {
+  console.log(number + " " + "is negative");
+}
