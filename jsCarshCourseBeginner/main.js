3b1e0e2527e01f9bb84bd1eb1a50e9d51a2f9543
AliAksoyy <alifeyza4017@gmail.com>
2023-09-25 10:33:03 +0300

    #1 jsCrashCourse  contuine

---
+++ jsCarshCourseBeginner/main.js
@@ -0,0 +1,35 @@
+console.log("object");
+
+let age = 25;
+
+const salary = 80000;
+
+// primitive
+// string, number, boolean,undefined, null,bigInıt, symbol,
+// non-primitive
+//  object
+
+const name = "ali";
+
+const total = 0;
+
+const isNewUser = false;
+
+let result;
+
+const res = undefined;
+
+const data = null;
+
+// null; js'de boş veya bilinmeyen değeri temsil eden özel bir değerdir
+
+// bigInıt; sayı veri türünün tutabileceğinden daha büyük bir tamsayı değerini belirtmek için
+
+// symbol; benzersiz ve değiştirilmez bir değeri ifade eder
+
+const person = {
+  firstName: "ali",
+  lastName: "aksoy",
+  age: 30,
+  isMarried: true,
+};
